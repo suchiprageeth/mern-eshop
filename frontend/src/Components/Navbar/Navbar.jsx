5699c5050a5bbdb2f28eb67ef04a150989a1f1e7
Suchiru Prageeth <suchiruprageeth94@gmail.com>
2025-08-01 14:14:23 +0530

    Add routing and page components

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
@@ -13,19 +14,38 @@ const Navbar = () => {
       </div>
       <ul className="nav-menu">
         <li onClick={() => setMenu("shop")}>
-          Shop {menu === "shop" && <hr />}
+          <Link className="link" to="/">
+            Shop
+          </Link>{" "}
+          {menu === "shop" && <hr />}
         </li>
-        <li onClick={() => setMenu("men")}>Men {menu === "men" && <hr />}</li>
+        <li onClick={() => setMenu("men")}>
+          <Link className="link" to="/men">
+            Men{" "}
+          </Link>
+          {menu === "men" && <hr />}
+        </li>
         <li onClick={() => setMenu("women")}>
-          Women {menu === "women" && <hr />}
+          <Link className="link" to="/women">
+            Women
+          </Link>{" "}
+          {menu === "women" && <hr />}
         </li>
         <li onClick={() => setMenu("kids")}>
-          Kids {menu === "kids" && <hr />}
+          <Link className="link" to="/kids">
+            Kids
+          </Link>{" "}
+          {menu === "kids" && <hr />}
         </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="Cart Icon" />
+        <Link className="link" to="/login">
+          <button>Login</button>
+        </Link>
+
+        <Link className="link" to="/cart">
+          <img src={cart_icon} alt="Cart Icon" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
