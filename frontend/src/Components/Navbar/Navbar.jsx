7c4e43e876ba16ac9e24730e9deea1c7852a6068
Suchiru Prageeth <suchiruprageeth94@gmail.com>
2025-08-01 13:51:57 +0530

    Implement navbar functionality with styling

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
 import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
+import { useState } from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [menu, setMenu] = useState("shop");
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" />
+        <p>E-Shop</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          Shop {menu === "shop" && <hr />}
+        </li>
+        <li onClick={() => setMenu("men")}>Men {menu === "men" && <hr />}</li>
+        <li onClick={() => setMenu("women")}>
+          Women {menu === "women" && <hr />}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          Kids {menu === "kids" && <hr />}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="Cart Icon" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
