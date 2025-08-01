f789a2825b0693fec507d10afff4b47a4352f0ad
Suchiru Prageeth <suchiruprageeth94@gmail.com>
2025-08-01 15:45:04 +0530

    Add Popular component with styling and item mapping

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -6,8 +6,8 @@ export const Item = ({ image, name, new_price, old_price }) => {
       <img src={image} alt="Image" />
       <p>{name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{new_price}</div>
-        <div className="item-price-old">{old_price}</div>
+        <div className="item-price-new">${new_price}</div>
+        <div className="item-price-old">${old_price}</div>
       </div>
     </div>
   );
