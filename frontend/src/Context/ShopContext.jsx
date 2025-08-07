39ec5298b3cb8922397e58a666a7b3faec066bf8
Suchiru Prageeth <suchiruprageeth94@gmail.com>
2025-08-07 22:31:24 +0530

    Add banners to ShopCategory and update context variable naming

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,10 @@
 import { createContext } from "react";
-import all_product from "../Components/Assets/all_product";
+import all_products from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-  const contextValue = { all_product };
+  const contextValue = { all_products };
 
   return (
     <ShopContext.Provider value={contextValue}>
--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useContext } from "react";
+import "./Styles/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import { Item } from "../Components/Item/Item";
 
-const ShopCategory = () => {
-  return <div>ShopCategory</div>;
+const ShopCategory = ({ category, banner }) => {
+  console.log(8, { category });
+
+  const { all_products } = useContext(ShopContext);
+
+  return (
+    <div className="shop-category">
+      <img className="shop-category-banner" src={banner} alt="Banner Logo" />
+      <div className="shop-category-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shop-category-sort">
+          Sort by <img src={dropdown_icon} alt="Dropdown Icon" />
+        </div>
+      </div>
+      <div className="shop-category-products">
+        {all_products.map((item, index) => {
+          if (category === item.category) {
+            return (
+              <Item
+                key={index}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+      <div className="shop-category-loadMore">
+        Explore More
+      </div>
+    </div>
+  );
 };
 
 export default ShopCategory;
